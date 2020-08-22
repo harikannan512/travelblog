a35c261da2ad1fa99fcd38bd80a557f9d905d475
Kaizukamezi <harikannan512@gmail.com>
2020-08-22 18:53:20 +0530

    user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Bloguser(User):
+    username = User.username
+    first = User.first_name
+    last = User.last_name
+    email = User.email
+    password = User.password
+
+    def register_user(self):
+        User.objects.create_user(self.username, self.email, self.password)
+
+
+class PostSubmission(models.Model):
+    title = models.CharField(max_length=160)
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
