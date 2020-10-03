a11f173110c5a6c4f68aa3ffddae8212093b4616
Kaizukamezi <harikannan512@gmail.com>
2020-10-03 18:46:59 +0530

    req/proc/settings

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +146,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
+
+django_heroku.settings(locals())
